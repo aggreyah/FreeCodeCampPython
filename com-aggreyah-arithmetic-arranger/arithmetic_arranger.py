d7b3d0e09cf5cf29e1a321c23b7720a77300cd2b
aggreyah <aggreyah@gmail.com>
2020-10-27 23:12:11 +0300

    arithmetic arranger fully functional

---
+++ com-aggreyah-arithmetic-arranger/arithmetic_arranger.py
@@ -0,0 +1,67 @@
+def arithmetic_arranger(problems, evaluate = None):
+    # problems are a string list with basic math problems.
+    if len(problems) > 5:
+        return "Error: Too many problems."
+    arranged_problems = ""
+    first_operands = []
+    second_operands = []
+    dashes_list = []
+    results = []
+
+    for i in range(len(problems)):
+        underline_string = '-'
+        operands_and_operator = problems[i].split(" ")
+
+        if not (operands_and_operator[1] == "+" or operands_and_operator[1] == "-"):
+            return "Error: Operator must be '+' or '-'."
+        elif (not operands_and_operator[0].isdigit()) or (not operands_and_operator[2].isdigit()):
+            return "Error: Numbers must only contain digits."
+        elif (len(operands_and_operator[0]) > 4) or (len(operands_and_operator[2]) > 4):
+            return "Error: Numbers cannot be more than four digits."
+
+        longest_operand = operands_and_operator[0] if len(operands_and_operator[0]) >= len(operands_and_operator[2]) \
+            else operands_and_operator[2]
+        if longest_operand == operands_and_operator[0]:
+            first_operands.append(f"{operands_and_operator[0].rjust(len(operands_and_operator[0]) + 2)}")
+        else:
+            first_operands.append(f"{operands_and_operator[0].rjust(len(longest_operand) + 2)}")
+        second_operands.append(f"{operands_and_operator[1]}{operands_and_operator[2].rjust(len(longest_operand) + 1)}")
+        if evaluate:
+            results.append(f"{str(eval(problems[i])).rjust(len(longest_operand) + 2)}")
+        underline_string *= (len(longest_operand) + 2)
+        dashes_list.append(f"{underline_string}")
+
+    arranged_problems += add_formatted_operands(first_operands, len(problems))
+
+    arranged_problems += add_formatted_operands(second_operands, len(problems))
+
+    arranged_problems += add_formatted_dashes(dashes_list, len(problems))
+
+    if evaluate:
+        arranged_problems += f"\n"
+        arranged_problems += add_formatted_dashes(results, len(problems))
+    return arranged_problems
+
+
+def add_formatted_dashes(dashes, num):
+    formatted_dashes = ""
+    for i in range(num):
+        if i < num - 1:
+            formatted_dashes += dashes[i] + "    "
+        else:
+            formatted_dashes += f"{dashes[i]}"
+    return formatted_dashes
+
+
+def add_formatted_operands(operand_list, size):
+    formatted_operands = ""
+    for i in range(size):
+        if i < size - 1:
+            formatted_operands += operand_list[i] + "    "
+        else:
+            formatted_operands += f"{operand_list[i]}\n"
+    return formatted_operands
+
+
+if __name__ == "__main__":
+    print(arithmetic_arranger(["3 + 855", "3801 - 2", "45 + 43", "123 + 49"], True))
