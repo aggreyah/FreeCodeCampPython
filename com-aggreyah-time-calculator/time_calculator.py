360805e1b0983262470940be88908d2e5c350817
aggreyah <aggreyah@gmail.com>
2020-10-28 13:23:06 +0300

    added fuctions to extract hours, minutes and the period given a time string.

--- com-aggreyah-time-calculator/time_calculator.py
+++ com-aggreyah-time-calculator/time_calculator.py
@@ -1,7 +1,23 @@
-def add_time(start, duration):
-
-
-
-
+def add_time(start, duration, day_of_week=None):
+    start_time_hour, start_time_minute, start_time_period = extract_hour_minute_and_period(start)
+    elapsed_time_hours, elapsed_time_minutes = extract_hour_minute_and_period(duration)
+    new_time = ""
 
     return new_time
+
+
+def extract_hour_minute_and_period(time_string):
+    start_time_and_period = time_string.split(" ")
+    time_hour, time_minute = get_hour_and_minute(start_time_and_period[0])
+    if len(start_time_and_period) == 1:
+        return time_hour, time_minute
+    else:
+        time_period = start_time_and_period[1]
+    return time_hour, time_minute, time_period
+
+
+def get_hour_and_minute(hour_and_minute):
+    start_time_hour_and_minute = hour_and_minute[0].split(":")
+    time_hour = start_time_hour_and_minute[0]
+    time_minute = start_time_hour_and_minute[1]
+    return time_hour, time_minute
