a209e49eea3402b5c462fb150d2a629f1ebaa399
aggreyah <aggreyah@gmail.com>
2020-11-03 18:32:51 +0300

    all tests passing except the create_spend_chart function.

--- com-aggreyah-budget-app/budget.py
+++ com-aggreyah-budget-app/budget.py
@@ -1,3 +1,10 @@
+FOUR = 4
+FIVE_SPACES = "     "
+FIVE = 5
+TEN = 10
+PERCENT = 100
+
+
 class Category:
     def __init__(self, category):
         self.category = category
@@ -35,6 +42,14 @@ class Category:
         else:
             return True
 
+    def get_total_withdrawals(self):
+        total_withdrawals = 0
+        for item in self.ledger:
+            item_amount = item["amount"]
+            if item_amount < 0:
+                total_withdrawals += -item_amount
+        return total_withdrawals
+
     def __str__(self):
         my_string = f"{self.category}".center(30, "*")
         category_total = 0
@@ -53,5 +68,99 @@ class Category:
         return my_string
 
 
+def get_expenditure(categories):
+    tot_expense = 0
+    for category in categories:
+        tot_expense += category.get_total_withdrawals()
+    return tot_expense
+
+
+def get_round_to_ten(value):
+    temp = value
+    if temp % TEN < FIVE:
+        temp = temp - (temp % TEN)
+    else:
+        temp = temp + (TEN - (temp % TEN))
+    return temp
+
+
+def find_longest_category(categories):
+    length = 0
+    for category in categories:
+        if len(category.category) > length:
+            length = len(category.category)
+    return length
+
+
+def print_category_names(categories):
+    formatted_categories = ""
+    longest_category = find_longest_category(categories)
+    j = 0
+    while j < longest_category:
+        current_line = ""
+        for category in categories:
+            good_category_name = category.category.capitalize()
+            if j < len(category.category):
+                if categories[0] == category:
+                    current_line += f"     {good_category_name[j]}"
+                elif categories[-1] == category:
+                    if j != longest_category - 1:
+                        current_line += f"  {good_category_name[j]}  \n"
+                    else:
+                        current_line += f"  {good_category_name[j]}  "
+                else:
+                    current_line += f"  {good_category_name[j]}"
+            else:
+                if categories[0] == category:
+                    current_line += f"      "
+                elif categories[-1] == category:
+                    current_line += f"   \n"
+                else:
+                    current_line += f"   "
+        formatted_categories += current_line
+        j += 1
+
+    return formatted_categories
+
+
 def create_spend_chart(categories):
-    pass
+    total_expenditure_categories = get_expenditure(categories)
+    percentage_string = "Percentage spent by category\n"
+    percent = PERCENT
+    bar_char = "o"
+    line_length = 0
+    while percent >= 0:
+        percent_string = str(percent) + "| "
+        if len(percent_string) < 5:
+            percent_string = percent_string.rjust(5, " ")
+        for category in categories:
+            category_percent = \
+                get_round_to_ten(int(PERCENT * (category.get_total_withdrawals() / total_expenditure_categories)))
+            if percent > category_percent:
+                percent_string += "   "
+            else:
+                percent_string += f"{bar_char}  "
+
+        percentage_string += (percent_string + "\n")
+        if percent == PERCENT:
+            line_length = len(percent_string)
+        percent -= 10
+    underlined = "-" * (line_length - FOUR)
+    underlined = underlined.rjust(line_length, " ")
+    percentage_string += underlined
+    category_names = print_category_names(categories)
+
+    return percentage_string + "\n" + category_names
+
+
+if __name__ == "__main__":
+    food_category = Category("food")
+    entertainment_category = Category("entertainment")
+    business_category = Category("business")
+    food_category.deposit(900, "deposit")
+    entertainment_category.deposit(900, "deposit")
+    business_category.deposit(900, "deposit")
+    food_category.withdraw(105.55)
+    entertainment_category.withdraw(33.40)
+    business_category.withdraw(10.99)
+    print(create_spend_chart([business_category, food_category, entertainment_category]))
--- com-aggreyah-time-calculator/time_calculator.py
+++ com-aggreyah-time-calculator/time_calculator.py
@@ -5,6 +5,8 @@ TIME_PERIOD_LENGTH = 12
 MINUTES_IN_HOUR = 60
 DAYS_OF_THE_WEEK = {"monday": 0, "tuesday": 1, "wednesday": 2, "thursday": 3, "friday": 4, "saturday": 5, "sunday": 6}
 TIME_PERIODS_IN_DAY = {0: "AM", 1: "PM"}
+
+
 def add_time(start, duration, day_of_week=None):
     number_of_days_later = 0
     period_transition = False
@@ -31,7 +33,7 @@ def add_time(start, duration, day_of_week=None):
         new_time = simple_add_time(start.split(" ")[0], new_duration_time)
     elif the_bigger_time(duration_to_transition, new_duration_time) == new_duration_time:
         period_transition = True
-        new_time = simple_add_time(TRANSITION_TIME, simple_subtract_time(duration_to_transition,new_duration_time))
+        new_time = simple_add_time(TRANSITION_TIME, simple_subtract_time(duration_to_transition, new_duration_time))
 
     end_period = start_period if not period_transition else (start_period + 1) % 2
     end_time_period_string = TIME_PERIODS_IN_DAY[end_period]
@@ -44,7 +46,7 @@ def add_time(start, duration, day_of_week=None):
     elif number_of_days_later > 1:
         number_of_days_later_string = f"{number_of_days_later} days later"
     if day_of_week:
-        end_day_of_the_week_int = (start_day_of_the_week_int  + number_of_days_later) % 7
+        end_day_of_the_week_int = (start_day_of_the_week_int + number_of_days_later) % 7
 
     if (number_of_days_later == 0) and (not day_of_week):
         f_new_time = f"{new_time} {end_time_period_string}"
@@ -68,11 +70,11 @@ def the_bigger_time(time_A, time_B):
     if eval(time_A_hours) > eval(time_B_hours):
         return time_A
     elif eval(time_B_hours) > eval(time_A_hours):
-        return  time_B
+        return time_B
     elif eval(time_A_hours) == eval(time_B_hours) and eval(time_A_minutes) > eval(time_B_minutes):
         return time_A
     elif eval(time_A_hours) == eval(time_B_hours) and eval(time_B_minutes) > eval(time_A_minutes):
-        return  time_B
+        return time_B
     else:
         return None
 
@@ -125,7 +127,6 @@ def simple_subtract_time(start, end):
     resulting_time_hour = f"0{resulting_time_hour}" if len(resulting_time_hour) < 2 else resulting_time_hour
     resulting_time_minute = f"0{resulting_time_minute}" if len(resulting_time_minute) < 2 else resulting_time_minute
     return f"{resulting_time_hour}:{resulting_time_minute}"
-
 
 
 def process_start_and_end_times(stop, begin):
